d1d7c482eecbc4c38a510497d451a4c87877e2ee
odysssses <119633864+AverageN00bie@users.noreply.github.com>
2025-05-17 19:28:19 -0300

    conversões acertadas, corregimentos gerais

--- seguelinha2.ino
+++ seguelinha2.ino
@@ -1,116 +1,44 @@
-/////////////////////
-//IN 1 e 2: OUT2 
-//IN 3 e 4: OUT1
-#define dirR 7 // direção do motor direito: Para frente / Para trás (HIGH ou LOW)_ porta IN1 ponte H;
-#define mR 8 // velocidade do motor direito ( 0 a 255) _ Porta IN2 ponte H; 
-#define dirL 9 //direção do motor esquerdo: Para frente / Para trás (HIGH ou LOW)_ porta IN3 ponte H;
-#define mL 10 // velocidade do motor esquerdo ( 0 a 255) _ Porta IN4 ponte H;
+#include <autocalibralib.h>
+#include <L298NM.h>
+
+#include <EEPROM.h>
+
+
+#define M_E 9
+#define M_D 11
+#define DIR_E 8
+#define DIR_D 10
+
+// Criando instância dos motores
+L298NM robo(M_E, DIR_E, M_D, DIR_D);
 
 int vel = 127;
-int vel0 = 0;
 
 ///////////////////////
 
-const int ledverde = 4;
-const int ledverm = 3;
-#define sensorL A4
-#define sensorR A1
+LineSensor sensorE(A3, 3);  // Sensor IV esquerdo com LED no pino 3
+LineSensor sensorD(A1, 4); // Sensor IV direito com LED no pino 4
 
-int pretogL;
-int pretogR;
-char ivL;
-char ivR;
-
-////////////////////////
-
-char defcor(int val, int pretog) {
-  if (val <= pretog + 70) {
-    return 'p';
-  } else {
-    return 'b';
-  }
-}
-
-void pisca(int t, int led) {
-  for (int i = 0; i < t; i++) {
-    delay(1000);
-    digitalWrite(led, HIGH);
-    Serial.print("piscou: ");
-    switch (led) {case 3: Serial.println("vermelho"); break; case 4: Serial.println("verde");}
-    delay(50);
-    digitalWrite(led, LOW);
-  }
-}
-
-/////////////////////////
-
-void m() {
-  analogWrite(mL, vel);
-  analogWrite(mR, vel);
-}
-
-void mov(int t) {
-  digitalWrite(dirL, LOW);
-  digitalWrite(dirR, LOW);
-  m();
-  delay(t);
-}
-
-void turnL(int t) {
-  digitalWrite(dirL, HIGH);
-  digitalWrite(dirR, LOW);
-  m();  
-  delay(t);
-}
-
-void turnR(int t) {
-  digitalWrite(dirL, LOW);
-  digitalWrite(dirR, HIGH);
-  m();  
-  delay(t);
-}
-
-void back(int t) {
-  digitalWrite(dirL, HIGH);
-  digitalWrite(dirR, HIGH);
-  m();  
-  delay(t);
-}
-
-///////////////////////////
+///////////////////////
 
 void setup() {
-  pinMode(mL, OUTPUT);
-  pinMode(mR, OUTPUT);
-  pinMode(dirL, OUTPUT);
-  pinMode(dirR, OUTPUT);
-  
-  pinMode(ledverde, OUTPUT);
-  pinMode(ledverm, OUTPUT);
-  pinMode(sensorL, INPUT);
-  pinMode(sensorR, INPUT);
-
-  Serial.begin(9600);
-
-  pisca(3, ledverm); pretogL = analogRead(sensorL); pisca(1, ledverde);
-  pisca(3, ledverm); pretogR = analogRead(sensorR); pisca(1, ledverde);
+  //Este código depende de uma calibração já presente em outro código, crie um ou use o primeiro exemplo da biblioteca "autocalibralib.h"
+  sensorE.setThreshold(static_cast<int>(EEPROM.read(0)));
+  sensorD.setThreshold(static_cast<int>(EEPROM.read(1)));
 }
 
-////////////////////////////
+///////////////////////
 
 void loop() {
-  ivL = defcor(analogRead(sensorL), pretogL);
-  ivR = defcor(analogRead(sensorR), pretogR);
-
-  Serial.print("Sensor esquerdo: "); Serial.print(ivL); Serial.print(" // Sensor direito: "); Serial.println(ivR);
+  bool pretoESQUERDO = sensorE.isBlack();
+  bool pretoDIREITO = sensorD.isBlack();
   
-  if ((ivL == 'p') && (ivR == 'b')) { 
-    back(50);
-    turnL(200);
-  } else if((ivL == 'b') && (ivR == 'p')) {
-    turnR(200);
-    back(50);
+  //pretoNOME: O sensor lê preto. !pretoNOME: O sensor lê branco.
+  if ((pretoESQUERDO) && (!pretoDIREITO)) { 
+    robo.turnLeft(vel); delay(200);
+  } else if ((!pretoESQUERDO) && (pretoDIREITO)) {
+    robo.turnRight(vel); delay(200);
   } else {
-     mov(1);
+    robo.forward(vel);
   }
 }
